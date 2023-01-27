c63514827c0947195b5d449f2714391cd54ca36e
Arina Gladysheva <arina1110@mail.ru>
2023-01-27 15:08:29 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function toReadable (number) {
-  
-}
+    let res = '';
+    const arr1 = ['zero','one','two','three','four','five','six','seven','eight','nine'];
+    const arr2 = ['ten','eleven','twelve','thirteen', 'fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+    const arr3 = ['twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+    let num = String(number);
+    if (num.length === 1) {
+        res = arr1[number];
+    } else if (num.length === 2 && number < 20) {
+        res = arr2[number - 10];
+    } else if (number > 19 && number < 100 && num[1] === '0') {
+        res = arr3[Number(num[0]) - 2];
+    } else if (number > 20 && number < 100 && num[1] !== '0') {
+        res = arr3[Number(num[0]) - 2] + ' ' + arr1[num[1]];
+    } else if (number > 99 && number < 100 && num[1] !== '0')
+    return res;
+};
