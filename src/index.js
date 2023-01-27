3c469e11c54d5f7a0ff385dc9eb795a414877456
Arina Gladysheva <arina1110@mail.ru>
2023-01-27 16:58:57 +0300

    all

--- src/index.js
+++ src/index.js
@@ -12,6 +12,16 @@ module.exports = function toReadable (number) {
         res = arr3[Number(num[0]) - 2];
     } else if (number > 20 && number < 100 && num[1] !== '0') {
         res = arr3[Number(num[0]) - 2] + ' ' + arr1[num[1]];
-    } else if (number > 99 && number < 100 && num[1] !== '0')
+    } else if (num.length === 3 && num[1] === '0' && num[2] === '0') {
+        res = arr1[Number(num[0])] + ' ' + 'hundred';
+    } else if (num.length === 3 && num[1] !== '0' && num[1] !== '1' && num[2] === '0') {
+        res = arr1[Number(num[0])] + ' ' + 'hundred' + ' ' + arr3[Number(num[1]) - 2];
+    } else if (num.length === 3 && num[1] !== '0' && num[1] !== '1' && num[2] !== '0') {
+        res = arr1[Number(num[0])] + ' ' + 'hundred' + ' ' + arr3[Number(num[1]) - 2] + ' ' + arr1[Number(num[2])];
+    } else if (num.length === 3 && num[1] === '1') {
+        res = arr1[Number(num[0])] + ' ' + 'hundred' + ' ' + arr2[Number(num.slice(1, 4)) - 10];
+    } else if (num.length === 3  && num[1] === '0' && num[2] !== '0') {
+        res = arr1[Number(num[0])] + ' ' + 'hundred' + ' ' + arr1[Number(num[2])];
+    } 
     return res;
 };
